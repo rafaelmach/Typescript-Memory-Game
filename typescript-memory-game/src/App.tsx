010f409b71d8c54369c253a4b33d352692e20062
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2022-01-11 12:08:50 -0300

    Game Board Created

--- typescript-memory-game/src/App.tsx
+++ typescript-memory-game/src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from 'react'
+// Setup
+import { createBoard } from "./setup"
+import { shuffleArray } from './utils'
+// Types
+import { CardType } from './setup'
 // Styles
-import { Grid } from './App.styles';
+import { Grid } from './App.styles'
 
 const App = () => {
+console.log(createBoard())
+
   return (
     <div>
       <Grid>Start Here!</Grid>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
